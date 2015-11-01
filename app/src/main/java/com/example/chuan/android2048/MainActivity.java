7a6b2105fcfe6643ba06e019de2442617182144b
DONG <dongchuan55@gmail.com>
2015-11-01 19:41:13 +0100

    Add best score

--- app/src/main/java/com/example/chuan/android2048/MainActivity.java
+++ app/src/main/java/com/example/chuan/android2048/MainActivity.java
@@ -4,11 +4,14 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import com.example.chuan.android2048.util.ScoreMaster;
+
 
 public class MainActivity extends Activity {
 
     private int score = 0;
     private TextView tvScore;
+    private TextView tvBest;
     private static MainActivity mainActivity;
 
     public MainActivity() {
@@ -21,19 +24,26 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         tvScore = (TextView) findViewById(R.id.tvScore);
+        tvBest = (TextView) findViewById(R.id.tvBest);
+        tvBest.setText(ScoreMaster.readScore(this) + "");
     }
 
     public static MainActivity getMainActivity() {
         return mainActivity;
     }
 
+    /**
+     * Clear score. Before it, check whether it's the best score.
+     */
     public void clearScore() {
+        ScoreMaster.saveScore(this, score);
         score = 0;
         showScore();
     }
 
     public void showScore() {
         tvScore.setText(score + "");
+        tvBest.setText(ScoreMaster.readScore(this) + "");
     }
 
     public void addScore(int s) {
